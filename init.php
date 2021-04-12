000ad8fe430b4777725189b0ef84e4a9c783e539
Nazmul Sabuz <naz.sabuz@gmail.com>
2021-04-13 04:27:26 +0600

    init

---
+++ init.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @package           Wp_Dropzone
+ * @version           1.0.5
+ * @link              https://profiles.wordpress.org/nazsabuz/
+ *
+ * Plugin Name:       WP Dropzone
+ * Description:       Upload files into WordPress media library from front-end.
+ * Version:           1.0.5
+ * Author:            Nazmul Sabuz
+ * Author URI:        https://profiles.wordpress.org/nazsabuz/
+ */
+
+// If this file is called directly, abort.
+if (!defined('WPINC')) {
+	die;
+}
+
+/**
+ * Including the core plugin functions.
+ *
+ * @since    1.0.0
+ */
+require_once dirname(__file__) . '/includes/core-functions.php';
