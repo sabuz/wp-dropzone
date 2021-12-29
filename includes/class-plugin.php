5c1dc340bbd17ecf2ef0e0b82ef49e7d82af96f8
Nazmul Sabuz <naz.sabuz@gmail.com>
2021-12-29 13:19:56 +0600

    wip: revamp

--- includes/class-plugin.php
+++ includes/class-plugin.php
@@ -50,7 +50,7 @@ class Plugin {
 	}
 
 	/**
-	 * Register the required assets files for this plugin.
+	 * Register the required asset files for this plugin.
 	 *
 	 * @since 1.0.0
 	 * @return void
@@ -58,5 +58,80 @@ class Plugin {
 	public function register_scripts() {
 		wp_register_style( 'dropzone', $this->url . 'css/dropzone.min.css', array(), '1.0.8' );
 		wp_register_script( 'dropzone', $this->url . 'js/dropzone.min.js', array(), '1.0.8', true );
+	}
+
+	/**
+	 * Handle ajax file upload to media library.
+	 *
+	 * @since    1.0.0
+	 */
+	public function wp_dz_ajax_upload_handle() {
+		if ( ! wp_verify_nonce( $_POST['wp_dz_nonce'], 'wp_dz_protect' ) ) {
+			return;
+		}
+
+		$message = array(
+			'error' => true,
+			'data'  => __( 'no file to upload.', 'wp-dropzone' ),
+		);
+
+		if ( ! empty( $_FILES ) ) {
+			$file = array(
+				'name' => isset( $_FILES['file']['name'] ) ? $_FILES['file']['name'] : '',
+				'type' => isset( $_FILES['file']['type'] ) ? $_FILES['file']['type'] : '',
+				'size' => isset( $_FILES['file']['size'] ) ? $_FILES['file']['size'] : 0,
+			);
+
+			// include file library if not exist.
+			if ( ! function_exists( 'wp_handle_upload' ) ) {
+				require_once ABSPATH . 'wp-admin/includes/file.php';
+			}
+
+			// fire hook before upload.
+			do_action( 'wp_dropzone_before_upload_file', $file );
+
+			// upload file to server.
+			$movefile = wp_handle_upload( $_FILES['file'], array( 'test_form' => false ) );
+
+			// if upload success & no error.
+			if ( $movefile && ! isset( $movefile['error'] ) ) {
+				// fire hook after upload.
+				do_action( 'wp_dropzone_after_upload_file', $file );
+
+				$filename      = $movefile['file'];
+				$filetype      = wp_check_filetype( basename( $filename ), null );
+				$wp_upload_dir = wp_upload_dir();
+
+				$attachment = array(
+					'guid'           => $wp_upload_dir['url'] . '/' . basename( $filename ),
+					'post_mime_type' => $filetype['type'],
+					'post_title'     => preg_replace( '/\.[^.]+$/', '', basename( $filename ) ),
+					'post_content'   => '',
+					'post_status'    => 'inherit',
+				);
+
+				// add file to media.
+				$attach_id = wp_insert_attachment( $attachment, $filename );
+
+				// fire hook after insert media.
+				do_action( 'wp_dropzone_after_insert_attachment', $attach_id );
+
+				// if attachment success.
+				if ( $attach_id ) {
+					require_once ABSPATH . 'wp-admin/includes/image.php';
+
+					// update attachment metadata.
+					$attach_data = wp_generate_attachment_metadata( $attach_id, $filename );
+					wp_update_attachment_metadata( $attach_id, $attach_data );
+				}
+
+				$message['error'] = false;
+				$message['data']  = wp_get_attachment_url( $attach_id );
+			} else {
+				$message['data'] = $movefile['error'];
+			}
+		}
+
+		wp_send_json( $message );
 	}
 }
